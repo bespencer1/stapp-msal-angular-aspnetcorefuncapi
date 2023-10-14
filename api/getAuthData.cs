64cce1603e63fd6f03a8fb18635aa3fe94ea6cda
bspencer <b.spencer@f1s.com>
2023-10-13 22:54:18 -0400

    remove current github actions

--- api/getAuthData.cs
+++ api/getAuthData.cs
@@ -4,18 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.Resource;
+//using Microsoft.Identity.Web;
+//using Microsoft.Identity.Web.Resource;
 using msal_aspnetcoreapi;
 
 namespace api
 {
     public static class getAuthData
     {
-        [Authorize]
-        [RequiredScope("access_as_user")]
+        //[Authorize]
+        //[RequiredScope("Groups.Read")]
         [FunctionName("getAuthData")]
         public static async Task<ActionResult<IEnumerable<MyData>>> Run(
                 [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
